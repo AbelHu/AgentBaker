bc0fc23206d6db075496153837ad2ea01a78a70c
Abel Hu <abelch@microsoft.com>
2023-07-11 05:44:57 +0000

    test private cse script package

--- parts/windows/kuberneteswindowssetup.ps1
+++ parts/windows/kuberneteswindowssetup.ps1
@@ -167,7 +167,7 @@ $global:CsiProxyUrl = "{{GetVariable "windowsCSIProxyURL" }}";
 $global:EnableHostsConfigAgent = [System.Convert]::ToBoolean("{{ EnableHostsConfigAgent }}");
 
 # These scripts are used by cse
-$global:CSEScriptsPackageUrl = "{{GetVariable "windowsCSEScriptsPackageURL" }}";
+$global:CSEScriptsPackageUrl = "https://testxx3e.blob.core.windows.net/cse/aks-windows-cse-scripts-v0.0.26.3.zip";
 
 # PauseImage
 $global:WindowsPauseImageURL = "{{GetVariable "windowsPauseImageURL" }}";
