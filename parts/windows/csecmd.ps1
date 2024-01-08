53551c918e51daffd5b3e2aa76232a8fa39933cc
Abel Hu <abelch@microsoft.com>
2024-01-08 09:46:46 +0000

    test

--- parts/windows/csecmd.ps1
+++ parts/windows/csecmd.ps1
@@ -14,8 +14,9 @@ $arguments = '
 -NetworkAPIVersion 2018-08-01
 -LogFile %SYSTEMDRIVE%\AzureData\CustomDataSetupScript.log
 -CSEResultFilePath %SYSTEMDRIVE%\AzureData\CSEResult.log';
+$inputFile = '%SYSTEMDRIVE%\AzureData\CustomData.bin';
 $outputFile = '%SYSTEMDRIVE%\AzureData\CustomDataSetupScript.ps1';
-if (!(Test-Path $inputFile)) { exit 49; };
+if (!(Test-Path $inputFile)) { Write-Error 'ErrorCode: `"WINDOWS_CSE_ERROR_NO_CUSTOM_DATA_BIN`", Error: `"%SYSTEMDRIVE%\AzureData\CustomData.bin does not exist.`"'; exit 49; };
 Copy-Item $inputFile $outputFile;
 Invoke-Expression('{0} {1}' -f $outputFile, $arguments);
 \"; if (!(Test-Path %SYSTEMDRIVE%\AzureData\CSEResult.log)) { Write-Error 'ErrorCode: \"WINDOWS_CSE_ERROR_NO_CSE_RESULT_LOG\", Error: \"%SYSTEMDRIVE%\AzureData\CSEResult.log does not exist.\"'; exit 50; }; $code=(Get-Content %SYSTEMDRIVE%\AzureData\CSEResult.log); exit $code
--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -7517,8 +7517,9 @@ $arguments = '
 -NetworkAPIVersion 2018-08-01
 -LogFile %SYSTEMDRIVE%\AzureData\CustomDataSetupScript.log
 -CSEResultFilePath %SYSTEMDRIVE%\AzureData\CSEResult.log';
+$inputFile = '%SYSTEMDRIVE%\AzureData\CustomData.bin';
 $outputFile = '%SYSTEMDRIVE%\AzureData\CustomDataSetupScript.ps1';
-if (!(Test-Path $inputFile)) { exit 49; };
+if (!(Test-Path $inputFile)) { Write-Error 'ErrorCode: `+"`"+`"WINDOWS_CSE_ERROR_NO_CUSTOM_DATA_BIN`+"`"+`", Error: `+"`"+`"%SYSTEMDRIVE%\AzureData\CustomData.bin does not exist.`+"`"+`"'; exit 49; };
 Copy-Item $inputFile $outputFile;
 Invoke-Expression('{0} {1}' -f $outputFile, $arguments);
 \"; if (!(Test-Path %SYSTEMDRIVE%\AzureData\CSEResult.log)) { Write-Error 'ErrorCode: \"WINDOWS_CSE_ERROR_NO_CSE_RESULT_LOG\", Error: \"%SYSTEMDRIVE%\AzureData\CSEResult.log does not exist.\"'; exit 50; }; $code=(Get-Content %SYSTEMDRIVE%\AzureData\CSEResult.log); exit $code`)
@@ -8064,6 +8065,7 @@ finally
     if (!($global:KubeBinariesPackageSASURL.Contains("1.26.6"))) {
         echo $global:ExitCode | Out-File -FilePath $CSEResultFilePath -Encoding utf8
     }
+
     Stop-Transcript
 
     # Remove the parameters in the log file to avoid leaking secrets
