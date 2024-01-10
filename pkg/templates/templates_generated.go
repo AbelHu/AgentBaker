089f86ddb0e035a22367cb86d86c6b0d12ed5452
Abel Hu <abelch@microsoft.com>
2024-01-10 10:17:52 +0000

    generate

--- pkg/templates/templates_generated.go
+++ pkg/templates/templates_generated.go
@@ -7519,10 +7519,10 @@ $arguments = '
 -CSEResultFilePath %SYSTEMDRIVE%\AzureData\CSEResult.log';
 $inputFile = '%SYSTEMDRIVE%\AzureData\CustomData.bin';
 $outputFile = '%SYSTEMDRIVE%\AzureData\CustomDataSetupScript.ps1';
-if (!(Test-Path $inputFile)) { Write-Error 'ErrorCode: `+"`"+`"WINDOWS_CSE_ERROR_NO_CUSTOM_DATA_BIN`+"`"+`", Error: `+"`"+`"%SYSTEMDRIVE%\AzureData\CustomData.bin does not exist.`+"`"+`"'; exit 49; };
+if (!(Test-Path $inputFile)) { Write-Error 'ErrorCode: `+"`"+`\"WINDOWS_CSE_ERROR_NO_CUSTOM_DATA_BIN`+"`"+`\", Error: `+"`"+`\"%SYSTEMDRIVE%\AzureData\CustomData.bin does not exist.`+"`"+`\"'; exit 49; };
 Copy-Item $inputFile $outputFile;
 Invoke-Expression('{0} {1}' -f $outputFile, $arguments);
-\"; if (!(Test-Path %SYSTEMDRIVE%\AzureData\CSEResult.log)) { Write-Error 'ErrorCode: \"WINDOWS_CSE_ERROR_NO_CSE_RESULT_LOG\", Error: \"%SYSTEMDRIVE%\AzureData\CSEResult.log does not exist.\"'; exit 50; }; $code=(Get-Content %SYSTEMDRIVE%\AzureData\CSEResult.log); exit $code`)
+\"; if (!(Test-Path %SYSTEMDRIVE%\AzureData\CSEResult.log)) { Write-Error 'ErrorCode: `+"`"+`\"WINDOWS_CSE_ERROR_NO_CSE_RESULT_LOG`+"`"+`\", Error: `+"`"+`\"%SYSTEMDRIVE%\AzureData\CSEResult.log does not exist.`+"`"+`\"'; exit 50; }; $code=(Get-Content %SYSTEMDRIVE%\AzureData\CSEResult.log); exit $code`)
 
 func windowsCsecmdPs1Bytes() ([]byte, error) {
 	return _windowsCsecmdPs1, nil
